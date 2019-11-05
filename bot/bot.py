05141505e9e2352d82f3573a79dd74031eebdbfd
Leonhard Lie <ll040@hdm-stuttgart.de>
2019-11-05 12:20:51 +0100

    Add check for added scores when updating

--- bot/bot.py
+++ bot/bot.py
@@ -19,7 +19,7 @@ class CriticAggregatorBot:
 
     def __init__(self):
         self.submit = {}
-        self.update = {}
+        self.edit = {}
         self.submissions_save = {'submission_id': np.array([]), 'time': np.array([]),
                                  'comment_id': np.array([]), 'aggregator': np.array([])}
         self.reddit = praw.Reddit('opencritic_bot')
@@ -28,35 +28,36 @@ class CriticAggregatorBot:
             with open('resources/submissions_save.pkl', 'rb') as file:
                 self.submissions_save = pickle.load(file)
 
+
     def get_submissions(self):
         return self.submit
 
+
     def new_submissions(self):
-        for submission in self.subreddit.search('review thread', time_filter='week'):
+        for submission in self.subreddit.search('review thread', time_filter='day'):
             if submission.id not in self.submissions_save['submission_id']:
                 self.submit[submission.id] = submission
         if self.submit:
             return True
         return False
 
+
     def save_submissions(self, submissions):
         with open(CriticAggregatorBot.SAVE_PATH, 'wb') as file:
             pickle.dump(submissions, file, protocol=pickle.HIGHEST_PROTOCOL)
 
+
     def reply(self, aggregator='OpenCritic'):
         for id, submission in self.submit.copy().items():
+            time.sleep(2)
+            reply_body = generate_reply_body(submission, aggregator)
             try:
-                time.sleep(2)
-                reply_body = generate_reply_body(submission, aggregator)
-                try:
-                    comment = submission.reply(reply_body)
-                except praw.exceptions.APIException:
-                    print("Exception!")
-                    continue
-                self.submissions_save['comment_id'] = np.append(self.submissions_save['comment_id'], [comment.id])
-                del self.submit[id]
+                comment = submission.reply(reply_body)
             except praw.exceptions.APIException:
-                raise praw.exceptions.APIException
+                print("Exception!")
+                continue
+            self.submissions_save['comment_id'] = np.append(self.submissions_save['comment_id'], [comment.id])
+            del self.submit[id]
             self.submissions_save['submission_id'] = np.append(self.submissions_save['submission_id'], [submission.id])
             self.submissions_save['time'] = np.append(self.submissions_save['time'], [datetime.now()])
             self.submissions_save['aggregator'] = np.append(self.submissions_save['aggregator'], [aggregator])
@@ -64,19 +65,30 @@ class CriticAggregatorBot:
         self.submit = {}
 
 
-    def edit(self):
+    def update(self):
         for sid, cid, agg in zip(self.submissions_save['submission_id'], self.submissions_save['comment_id'], self.submissions_save['aggregator']):
-            time.sleep(2)
+            comment = self.reddit.comment(cid)
             reply_body = generate_reply_body(self.reddit.submission(sid), agg)
-            #self.reddit.comment(cid).edit(reply_body)  # TODO: check if body has changed
+            try:
+                if comment.author is None:
+                    print('Comment was removed.')
+                    continue
+                if comment.body != reply_body:
+                    comment.edit(reply_body)
+                    print('Comment edited.')
+                else:
+                    print('No changes detected.')
+            except praw.exceptions.ClientException:
+                print('Comment not found.')
+                continue
 
 
-    def new_updates(self):
+    def recent_submissions(self):
         if len(self.submissions_save['submission_id']) == 0:
             return False
         keep_idx = [i for i, t in enumerate(self.submissions_save['time']) if ((t - datetime.now()).seconds / 3600) < 48]
         self.submissions_save['submission_id'] = self.submissions_save['submission_id'][keep_idx]
-        self.submissions_save['time'] = self.submissions_save['submission_id'][keep_idx]
-        self.submissions_save['comment_id'] = self.submissions_save['submission_id'][keep_idx]
+        self.submissions_save['time'] = self.submissions_save['time'][keep_idx]
+        self.submissions_save['comment_id'] = self.submissions_save['comment_id'][keep_idx]
         self.submissions_save['aggregator'] = self.submissions_save['aggregator'][keep_idx]
         return True
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bot.bot import CriticAggregatorBot
-import praw
-import time
 import os
+import pickle
+import numpy as np
 
 
 __author__ = 'https://github.com/lebeli'
@@ -14,12 +14,12 @@ __status__ = 'Dev'
 
 if __name__ == '__main__':
     criticAggregatorBot = CriticAggregatorBot()
-    # if criticAggregatorBot.new_updates():
-    #     print('New updates!')
-    #     criticAggregatorBot.edit()
+    if criticAggregatorBot.recent_submissions():
+        print('Checking for updates')
+        criticAggregatorBot.update()
     if criticAggregatorBot.new_submissions():
         print('New submissions!')
         criticAggregatorBot.reply(aggregator='MetaCritic')
     else:
-        print("No updates found.")
+        print('No submissions found.')
 
